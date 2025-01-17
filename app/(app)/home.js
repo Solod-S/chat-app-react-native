cb5ca8a6caa8c617a8075fbb8be8eab89bc7e686
Sergey <104456968+Solod-S@users.noreply.github.com>
2025-01-17 07:41:59 +0200

    added tailwind and started working on context auth logic
    added tailwind and started working on context auth logic

---
+++ app/(app)/home.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Home() {
+  return (
+    <View className="bg-slate-50">
+      <Text>home</Text>
+    </View>
+  );
+}
---
+++ app/_layout.js
@@ -0,0 +1,38 @@
+import { View, Text } from "react-native";
+import { Slot, useRouter, useSegments } from "expo-router";
+import { AuthContextProvider, useAuth } from "@/context/authContext";
+// Import your global CSS file
+import "../global.css";
+import { useEffect } from "react";
+
+const MainLayout = () => {
+  const { isAuthenticated } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    //  check if the user is authenticated or not
+    if (typeof isAuthenticated == "undefined") return;
+    // user in app group
+    const inApp = segments[0] == "(app)";
+    if (isAuthenticated && !inApp) {
+      // if user authenticated
+      // and not in (app) => redirect home
+      router.replace("home");
+    } else if (isAuthenticated == false) {
+      // if user is not authenticated
+      //  redirect to signIn
+      router.replace("signIn");
+    }
+  }, [isAuthenticated]);
+
+  return <Slot />;
+};
+
+export default function RootLayout() {
+  return (
+    <AuthContextProvider className="flex-1">
+      <MainLayout />
+    </AuthContextProvider>
+  );
+}
